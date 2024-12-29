d8cc1a8e4006867f9513867dca666fd1e4d0269c
Nobody <35265233+KickaStone@users.noreply.github.com>
2024-12-29 17:51:51 +0800

    在项目信息里添加模块信息

--- pages/project/[id].vue
+++ pages/project/[id].vue
@@ -4,15 +4,44 @@
       <span class="text-large font-600 mr-3"> 项目详情 </span>
     </template>
   </el-page-header>
-  
+
   <h2>项目信息</h2>
   <h3 v-if="project">项目ID: {{ project.id }}</h3>
   <h3 v-if="project">项目名称: {{ project.project_name }}</h3>
   <h3 v-if="project">产品类型: {{ project.product_type }}</h3>
   <h3 v-if="project">描述: {{ project.description }}</h3>
+  <el-divider />
 
-  TODO: 模块显示
-  </template>
+
+  <h2>模块信息</h2>
+  <!-- TODO: 模块信息可能还要封装一个组件 -->
+  <el-space :fill="false" direction="horizontal" wrap >
+    <el-card style="min-width: 300px; height: auto;" shadow="hover" v-for="idx in 10" :key="idx">
+      <template #header>
+        <div class="card-header">
+          模块名称{{ idx }}
+        </div>
+      </template>
+      <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+      <template #footer>Footer content</template>
+    </el-card>
+    
+  </el-space>
+
+  <el-divider />
+
+  <el-descriptions title="联系人">
+    <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
+    <el-descriptions-item label="Telephone">18100000000</el-descriptions-item>
+    <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
+    <el-descriptions-item label="Remarks">
+      <el-tag size="small">School</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item label="Address">
+      No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+    </el-descriptions-item>
+  </el-descriptions>
+</template>
 
 <script lang="ts" setup>
 import { useRouter, useRoute } from 'vue-router'
@@ -45,6 +74,4 @@ await axios.get(`/api/projects/${id}`).then((res) => {
 })
 </script>
 
-<style>
-
-</style>
+<style></style>
