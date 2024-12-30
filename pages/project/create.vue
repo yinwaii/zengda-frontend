bbf4571d6e858eba93218be9bbecef17bccef29d
Nobody <35265233+KickaStone@users.noreply.github.com>
2024-12-30 17:36:19 +0800

    添加创建项目api

--- pages/project/create.vue
+++ pages/project/create.vue
@@ -4,11 +4,11 @@
     <el-divider />
     <el-form>
       <el-form-item label="项目名称">
-        <el-input v-model="projectName" />
+        <el-input v-model="projectName" placeholder="请输入项目名称" />
       </el-form-item>
 
       <el-form-item label="项目编号">
-        <el-input v-model="projectId" />
+        <el-input v-model="projectId" placeholder="请输入项目编号" />
       </el-form-item>
 
       <el-form-item label="产品类型">
@@ -25,7 +25,7 @@
           style="width: 240px"
           :autosize="{ minRows: 2, maxRows: 4 }"
           type="textarea"
-          placeholder="Please input"
+          placeholder="请输入项目描述"
         />
       </el-form-item>
 
@@ -39,22 +39,30 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import { ArrowDown } from '@element-plus/icons-vue'
-
+import axios from 'axios'
 let projectName = ref('')
 let projectId = ref('')
 let productType = ref('')
 let description = ref('')
 
+
+// TODO: 从后端获取产品类型
 const options = [
   { value: '1', label: 'Option 1' },
   { value: '2', label: 'Option 2' },
   { value: '3', label: 'Option 3' },
 ]
 
+const createProject = async () => {
+  const project = {
+    projectName: projectName.value,
+    projectId: projectId.value,
+    productType: productType.value,
+    description: description.value,
+  }
 
-const createProject = () => {
-  // TODO: 创建项目
-  console.log(projectName.value, productType.value, description.value)
+  const res = await axios.post('/api/projects', project)
+  console.log(res)
 }
 </script>
 
