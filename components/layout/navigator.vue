aa00d70e66651cd6fc09e97fcd6ee0406505640b
yinwai <yinwai@sjtu.edu.cn>
2025-01-07 14:11:55 +0800

    调整页面结构

--- components/layout/navigator.vue
+++ components/layout/navigator.vue
@@ -3,11 +3,11 @@
 		<el-sub-menu index="/design">
 			<template #title>模板管理</template>
 			<el-menu-item index="/design/arguments">模块管理</el-menu-item>
-			<el-menu-item index="/design/algorithm">报价算法</el-menu-item>
+			<!-- <el-menu-item index="/design/algorithm">报价算法</el-menu-item> -->
 			<el-menu-item index="/design/product">产品模板</el-menu-item>
 			<el-menu-item index="/design/history">历史报价</el-menu-item>
 		</el-sub-menu>
-		<!-- <el-sub-menu index="/price">
+		<el-sub-menu index="/price">
 			<template #title>报价管理</template>
 			<el-menu-item index="/price/calculate">报价计算</el-menu-item>
 			<el-menu-item index="/price/edify">报价校核</el-menu-item>
@@ -26,7 +26,7 @@
 			<el-menu-item index="/manage/record">活动记录</el-menu-item>
 			<el-menu-item index="/manage/db">数据库管理</el-menu-item>
 			<el-menu-item index="/manage/log">系统日志</el-menu-item>
-		</el-sub-menu> -->
+		</el-sub-menu>
 	</el-menu>
 </template>
 
